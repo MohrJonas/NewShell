1a81afeca37b1f72ee32025091b3026dba69fb1e
Jonas Mohr <jones.m.987654321@gmail.com>
2021-07-06 18:42:42 +0200

    Added better return-values, added some nice colors to the terminal :D

--- src/main/java/shell/Executor.java
+++ src/main/java/shell/Executor.java
@@ -13,14 +13,14 @@ import static cTools.KernelWrapper.*;
 @UtilityClass
 public class Executor {
 
-    //[✅] [✅] cat test          # Inhalt der Datei test wird auf dem Terminal ausgegeben
-    //[✅] [✅] cat < test        # Inhalt der Datei test wird auf dem Terminal ausgegeben
-    //[✅] [✅] cat - < test      # Inhalt der Datei test wird auf dem Terminal ausgegeben
-    //[✅] [✅] cat test - test < test   # test wird drei mal ausgegeben
-    //[✅] [✅] cat test > xyz    # wirkt wie "cp test xyz"
-    //[❎] [❎] cat < test > xxx    # wirkt wie "cp test xxx"
-    //[❎] [❎] cat > yyy < test    # wirkt wie "cp test yyy"
-    //[❎] [❎] cat - > zzz < test  # wirkt wie "cp test zzz"
+    //[✅] cat test                  Inhalt der Datei test wird auf dem Terminal ausgegeben
+    //[✅] cat < test                Inhalt der Datei test wird auf dem Terminal ausgegeben
+    //[✅] cat - < test              Inhalt der Datei test wird auf dem Terminal ausgegeben
+    //[✅] cat test - test < test    test wird drei mal ausgegeben
+    //[✅] cat test > xyz            wirkt wie "cp test xyz"
+    //[✅] cat < test > xxx          wirkt wie "cp test xxx"
+    //[✅] cat > yyy < test          wirkt wie "cp test yyy"
+    //[✅] cat - > zzz < test        wirkt wie "cp test zzz"
     public int execute(List<TokenBlock> blocks) {
         log.info("Starting execution blocks are: " + blocks);
         final TokenBlock masterBlock = blocks.get(0);
@@ -64,34 +64,6 @@ public class Executor {
                 close(fd);
             }
         }
-        //for (final TokenBlock block : blocks) {
-        //    if (block.equals(masterBlock)) {
-        //        log.info("Block is MasterBlock. Skipping.");
-        //        continue;
-        //    }
-        //    if (block.getReadsFrom().size() == 0 && block.getWritesTo().size() == 0) {
-        //        log.info("Block neither writes nor reads nor is MasterBlock. Skipping.");
-        //        continue;
-        //    }
-        //    if (block.getReadsFrom().size() > 0) {
-        //        log.info("Block " + block + " reads from " + block.getReadsFrom().size() + " source");
-        //        block.getReadsFrom().forEach(b -> {
-        //            log.info("Creating Process between " + block + " and " + b);
-        //            createProcess(block.asCmd(), block.asArgs(), b.asCmd(), null, new int[2], STDIN_FILENO, STDOUT_FILENO);
-        //            log.info("Breaking connection between " + block + " and " + b);
-        //            breakConnection(masterBlock, b);
-        //        });
-        //    }
-        //    if (block.getWritesTo().size() > 0) {
-        //        log.info("Block " + block + " writes to " + block.getWritesTo().size() + " source");
-        //        block.getWritesTo().forEach(b -> {
-        //            log.info("Creating Process between " + block + " and " + b);
-        //            createProcess(block.asCmd(), block.asArgs(), null, b.asCmd(), new int[2], STDIN_FILENO, STDOUT_FILENO);
-        //            log.info("Breaking connection between " + block + " and " + b);
-        //            breakConnection(masterBlock, b);
-        //        });
-        //    }
-        //}
         log.info("Done executing blocks");
         return ExitCodes.OKAY;
     }
@@ -101,9 +73,6 @@ public class Executor {
         writer.setWritesTo(null);
     }
 
-    /*
-     * open returns file-descriptor pointing to file
-     */
     private int redirectOutput(String fOut, String fIn) {
         if (fOut != null) {
             close(STDOUT_FILENO);
@@ -119,66 +88,48 @@ public class Executor {
         return ExitCodes.OKAY;
     }
 
-    /**
-     * @param cmd       the command to run as absolute path f.e. /usr/bin/ls
-     * @param args      the args for the cmd the first arg is the file name f.e. ls
-     * @param fIn       The file to read from, relative
-     * @param fOut      The file to write to, relative
-     * @param pipefd    ?
-     * @param newStdin  ?
-     * @param newStdout ?
-     */
     public int createProcess(String cmd, String[] args, String fIn, String fOut, int[] pipefd, int newStdin, int newStdout) {
         final int[] status = new int[1];
         final int pid = fork();
         switch (pid) {
             case -1:
                 log.severe("Error forking");
-                exit(ExitCodes.TERMINATED);
-                break;
+                return ExitCodes.ERROR;
             case 0:
                 if (newStdin != STDIN_FILENO && newStdout != STDOUT_FILENO) {
                     close(newStdout);
                     dup2(newStdin, STDIN_FILENO);
                     close(newStdin);
-                    System.err.println("Set Pipe for Read");
                 }
                 if (fOut != null || fIn != null) {
                     if (redirectOutput(fOut, fIn) != 0) {
-                        System.err.println("Error redirecting output");
-                        return 1;
+                        log.severe("Error redirecting output");
+                        return ExitCodes.ERROR;
                     }
                 }
                 if (pipefd[0] != STDIN_FILENO && pipefd[1] != STDOUT_FILENO) {
-                    //Close read end
                     close(pipefd[0]);
-                    //Make STDOUT_FILENO point to pipefd[1]
                     dup2(pipefd[1], STDOUT_FILENO);
-                    //Close write end
                     close(pipefd[1]);
-                    System.err.println("Set Pipe for Write");
                 }
                 if (execv(cmd, args) < 0) {
-                    System.err.println("Error: execv");
-                    exit(1);
+                    log.severe("Error running program");
+                    exit(ExitCodes.TERMINATED);
                 }
             default:
                 if (newStdin != STDIN_FILENO && newStdout != STDOUT_FILENO) {
                     close(newStdin);
                     close(newStdout);
                 }
-
                 if (waitpid(pid, status, 0) < 0) {
-                    System.err.println("Error: waiting for child");
-                    exit(1);
+                    log.severe("Error while waiting for child");
+                    return ExitCodes.ERROR;
                 }
                 if (status[0] != 0) {
-                    System.err.println("Status: Child returned error code");
-                    return 1;
+                    log.severe("Status: Child returned error code");
+                    return status[0];
                 }
                 return ExitCodes.OKAY;
         }
-        System.out.println();
-        return ExitCodes.TERMINATED;
     }
 }
--- src/main/java/shell/Main.java
+++ src/main/java/shell/Main.java
@@ -6,29 +6,46 @@ import shell.tokenization.TokenBlock;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
-	private static final Stopwatch stopwatch = Stopwatch.createUnstarted();
-	private static int lastExitCode = 0;
-	private static long lastExecTime = 0L;
+    private static final Stopwatch stopwatch = Stopwatch.createUnstarted();
+    private static int lastExitCode = 0;
+    private static long lastExecTime = 0L;
 
-	public static void main(String[] args) {
-		while (true) {
-			System.out.println();
-			System.out.print(getHeader());
-			final List<TokenBlock> blocks = InputManager.readCommand();
-			System.out.println(blocks.toString());
-			stopwatch.start();
-			lastExitCode = Executor.execute(blocks);
-			stopwatch.stop();
-			lastExecTime = stopwatch.elapsed(TimeUnit.MILLISECONDS);
-			stopwatch.reset();
-		}
-	}
+    public static void main(String[] args) {
+        Logger.getLogger(Executor.class.getName()).setLevel(Level.WARNING);
+        while (true) {
+            System.out.println();
+            System.out.print(getHeader());
+            final List<TokenBlock> blocks = InputManager.readCommand();
+            stopwatch.start();
+            lastExitCode = Executor.execute(blocks);
+            stopwatch.stop();
+            lastExecTime = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+            stopwatch.reset();
+        }
+    }
 
-	private static String getHeader() {
-		final LocalDateTime now = LocalDateTime.now();
-		return String.format("%s@%d:%d:%d=%d=%dms=> ", System.getProperty("user.name"), now.getHour(), now.getMinute(), now.getSecond(), lastExitCode, lastExecTime);
-	}
+    private static String getHeader() {
+        final LocalDateTime now = LocalDateTime.now();
+        return String.format("%s%s%s@%s%d:%d:%d%s=%s%d%s=%s%d%sms=> ",
+                Colors.ANSI_BLUE,
+                System.getProperty("user.name"),
+                Colors.ANSI_RESET,
+                Colors.ANSI_CYAN,
+                now.getHour(),
+                now.getMinute(),
+                now.getSecond(),
+                Colors.ANSI_RESET,
+                Colors.ANSI_RED,
+                lastExitCode,
+                Colors.ANSI_RESET,
+                Colors.ANSI_GREEN,
+                lastExecTime,
+                Colors.ANSI_RESET
+        );
+    }
 }
